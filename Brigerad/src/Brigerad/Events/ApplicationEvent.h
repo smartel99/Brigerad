5b7a775cb3e29b9c3062ac460cc753a05d3698fb
Samuel Martel <martelsamuel00@gmail.com>
2020-02-24 16:21:21 -0500

    Updated .gitignore

--- Brigerad/src/Brigerad/Events/ApplicationEvent.h
+++ Brigerad/src/Brigerad/Events/ApplicationEvent.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include "Brigerad/Events/Event.h"
 
--- premake5.lua
+++ premake5.lua
@@ -15,7 +15,7 @@ project "Brigerad"
     kind "SharedLib"
     language "C++"
 
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    targetdire ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
     pchheader "brpch.h"

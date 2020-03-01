cfa32ec7bf8840c5048d99bc62c3b9f4b5ee8d58
Samuel Martel <martelsamuel00@gmail.com>
2020-03-01 13:10:10 -0500

    Key and Mouse Codes

--- Brigerad/src/Brigerad.h
+++ Brigerad/src/Brigerad.h
@@ -8,6 +8,10 @@
 #include "Brigerad/Log.h"
 #include "Brigerad/ImGui/ImGuiLayer.h"
 
+#include "Brigerad/Input.h"
+#include "Brigerad/KeyCodes.h"
+#include "Brigerad/MouseButtonCodes.h"
+
 // --- Entry Point ---
 #include "Brigerad/EntryPoint.h"
 // -------------------
